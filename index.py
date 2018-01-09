068bbd1d7b517b90824da3ce40658f08d28e6fb3
Tiago Rocha <tiagorocha@iMac-de-Tiago.local>
2018-01-09 16:59:29 -0200

    update function

--- index.py
+++ index.py
@@ -1,47 +1,51 @@
 import random
 
-print("==========================")
-print("Bem vindo ao Jogo da FORCA")
-print("==========================")
+def jogar():
+    print("==========================")
+    print("Bem vindo ao Jogo da FORCA")
+    print("==========================")
 
-file = open('file.txt', 'r')
+    file = open('file.txt', 'r')
 
-palavras = []
-for nome in file:
-    nome = nome.strip()
-    palavras.append(nome)
+    palavras = []
+    for nome in file:
+        nome = nome.strip()
+        palavras.append(nome)
 
-file.close()
+    file.close()
 
-numero = random.randrange(0, len(palavras))
+    numero = random.randrange(0, len(palavras))
 
-palavra_secreta = palavras[numero].upper()
-enforcou = False
-acertou = False
-erros = 0
-tracos = [ '_' for letra in palavra_secreta]
+    palavra_secreta = palavras[numero].upper()
+    enforcou = False
+    acertou = False
+    erros = 0
+    tracos = [ '_' for letra in palavra_secreta]
 
-while not enforcou and not acertou:
-    chute = input("Qual letra você escolhe?")
-    chute = chute.strip().upper()
+    while not enforcou and not acertou:
+        chute = input("Qual letra você escolhe?")
+        chute = chute.strip().upper()
 
-    if chute in palavra_secreta:
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                tracos[index] = letra
-            index += 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute.upper() == letra.upper():
+                    tracos[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == len(palavra_secreta)
+        acertou = '_' not in tracos
+        print(tracos)
+
+    if acertou:
+        print('VOCÊ GANHOU!!!')
     else:
-        erros += 1
+        print('VOCÊ PERDEU!!!')
 
-    enforcou = erros == len(palavra_secreta)
-    acertou = '_' not in tracos
-    print(tracos)
+    print('FIM DO JOGO!!!')
 
-if acertou:
-    print('VOCÊ GANHOU!!!')
-else:
-    print('VOCÊ PERDEU!!!')
-
-print('FIM DO JOGO!!!')
+if __name__ == "__main__":
+    jogar()
             
